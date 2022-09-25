d1b3db1f0282092e490d6edb8ee7e7d96595aa90
Paige <prhoward97@gmail.com>
2022-09-25 16:30:50 -0400

    commit tree d38703af3f9c28e66fcf0e72bd3c596bed57f27d

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+     callback();
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log("Idek anymore");
+    };
+}
+    
